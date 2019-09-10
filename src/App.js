2a8f447b51c2f6c8275a493d0feb2eaae08f2869
Scott (DC)  Christensen <schristensen10@gmail.com>
2019-09-09 19:18:58 -0600

    updating

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
+import List from "./List";
+import GroceryForm from "./GroceryForm";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    groceries: [
+      { id: 1, name: "Milk", complete: false, },
+      { id: 2, name: "Apples", complete: false, },
+      { id: 3, name: "Ice Cream", complete: false, },
+    ],
+  };
+
+  getUniqId = () => {
+    //NOTE We are just using this as a helper function for id's since we aren't using a db yet
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+   }
+
+   addItem = (name) => {
+     const {groceries} = this.state;
+     const grocery = { name, id: this.getUniqId(), complete: false}
+     this.setState({ groceries: [grocery, ...groceries]})
+   }
+
+   
+ 
+   handleClick = (id) => {
+     const { groceries } = this.state;
+     this.setState({
+       groceries: groceries.map( grocery => {
+         if (grocery.id === id) {
+           return {
+             ...grocery,
+             complete: !grocery.complete
+            }
+          }
+          return grocery
+        })
+      })
+    }
+
+    render() {
+      return (                             
+        <div>                             
+        <GroceryForm addItem={this.addItem} />
+        <List name="Grocery List" items={this.state.groceries} groceryClick={this.handleClick} />                                                          
+      </div>                             
+    );
+  }
+}                             
+                // render = () => {
+  //   const { groceries, } = this.state
+  //   return < List name="Grocery List" items={groceries} /> 
+  // }
+//   render (){
+//     return (
+//       <div>
+//         <ul>
+//         {this.renderGroceries()}
+//         </ul>
+//       </div>
+//     );
+//   };
+// };
 
 export default App;
